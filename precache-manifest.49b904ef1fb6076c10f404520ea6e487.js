288b79fe21b1c3f35135c1cb642159858e83f33d
Code Sandbox <hello@codesandbox.io>
2022-02-24 08:02:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.49b904ef1fb6076c10f404520ea6e487.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3534f716709149c975541b020d91cda",
+    "url": "https://fd-pixel.github.io/csb-7yfb7/index.html"
+  },
+  {
+    "revision": "2f1e30e6dacf2dfb73a7",
+    "url": "https://fd-pixel.github.io/csb-7yfb7/static/css/main.1b9ee248.chunk.css"
+  },
+  {
+    "revision": "55e408d6d551436298de",
+    "url": "https://fd-pixel.github.io/csb-7yfb7/static/js/2.36cbf796.chunk.js"
+  },
+  {
+    "revision": "2f1e30e6dacf2dfb73a7",
+    "url": "https://fd-pixel.github.io/csb-7yfb7/static/js/main.b27bbbf1.chunk.js"
+  },
+  {
+    "revision": "fdc508d955f36bcf9ac4",
+    "url": "https://fd-pixel.github.io/csb-7yfb7/static/js/runtime~main.44f183c6.js"
+  }
+]);
